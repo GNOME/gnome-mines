7c9244413425ddb7c357ded709cac32b0d4e0e66
Gediminas Paulauskas <menesis@delfi.lt>
2001-11-27 01:11:43 +0000

    define GETTEXT_PACKAGE use GETTEXT_PACKAGE and UTF-8.
    2001-11-27  Gediminas Paulauskas <menesis@delfi.lt>
    
    	* acconfig.h, configure.in: define GETTEXT_PACKAGE
    	* all games: use GETTEXT_PACKAGE and UTF-8.

--- gnomine.c
+++ gnomine.c
@@ -632,8 +632,9 @@ main (int argc, char *argv[])
 
 	gnome_score_init("gnomine");
 
-	bindtextdomain (PACKAGE, GNOMELOCALEDIR);
-	textdomain (PACKAGE);
+	bindtextdomain (GETTEXT_PACKAGE, GNOMELOCALEDIR);
+	bind_textdomain_codeset (GETTEXT_PACKAGE, "UTF-8");
+	textdomain (GETTEXT_PACKAGE);
 
         gnome_init_with_popt_table("gnomine", VERSION, argc, argv,
 				   options, 0, NULL);
