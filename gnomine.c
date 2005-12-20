2a1aaf19d449f6402af2915b31eabaad85f05c10
Callum McKenzie <callum@src.gnome.org>
2005-12-20 07:11:14 +0000

    setgid games and the new high scores code should work now.

--- gnomine.c
+++ gnomine.c
@@ -897,6 +897,8 @@ main (int argc, char *argv[])
 
 	gint width, height;
 
+	setgid_io_init ();
+
 	bindtextdomain (GETTEXT_PACKAGE, GNOMELOCALEDIR);
 	bind_textdomain_codeset (GETTEXT_PACKAGE, "UTF-8");
 	textdomain (GETTEXT_PACKAGE);
