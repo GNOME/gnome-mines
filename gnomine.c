ffdd158e4c641067c06c1a828930532c8d71b157
Thomas Hindoe Paaboel Andersen <thomashpa@src.gnome.org>
2008-10-13 21:33:41 +0000

    replace gnome_help_display with gtk_show_uri
    svn path=/trunk/; revision=8016

--- gnomine.c
+++ gnomine.c
@@ -798,7 +798,26 @@ preferences_callback (void)
 static void
 help_callback (void)
 {
-  gnome_help_display ("gnomine.xml", NULL, NULL);
+  GdkScreen *screen;
+  GError *error = NULL;
+
+  screen = gtk_widget_get_screen (GTK_WIDGET (window));
+  gtk_show_uri (screen, "ghelp:gnomine", gtk_get_current_event_time (), &error);
+
+  if (error != NULL)
+  {
+    GtkWidget *d;
+    d = gtk_message_dialog_new (GTK_WINDOW (window), 
+                              GTK_DIALOG_MODAL | GTK_DIALOG_DESTROY_WITH_PARENT,
+                              GTK_MESSAGE_ERROR, GTK_BUTTONS_CLOSE, 
+                              "%s", _("Unable to open help file"));
+    gtk_message_dialog_format_secondary_text (GTK_MESSAGE_DIALOG (d),
+                              "             %s", error->message);
+    g_signal_connect (d, "response", G_CALLBACK (gtk_widget_destroy), NULL);
+    gtk_window_present (GTK_WINDOW (d));
+
+    g_error_free (error);
+  }
 }
 
 const GtkActionEntry actions[] = {
