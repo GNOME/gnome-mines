7ebe16bc2f505675a712750442170bd6bac1bab5
Thomas Hindoe Paaboel Andersen <phomes@gmail.com>
2009-10-06 01:04:27 +0200

    GSEAL: fixes for GTK_WIDGET_HAS_FOCUS and GTK_WIDGET_VISIBLE
    bug#586731

--- minefield.c
+++ minefield.c
@@ -1272,7 +1272,7 @@ gtk_minefield_set_size (GtkMineField * mfield, guint xsize, guint ysize)
   mfield->xsize = xsize;
   mfield->ysize = ysize;
 
-  if (GTK_WIDGET_VISIBLE (mfield)) {
+  if (gtk_widget_get_visible (mfield)) {
     gtk_widget_queue_resize (GTK_WIDGET (mfield));
   }
 }
